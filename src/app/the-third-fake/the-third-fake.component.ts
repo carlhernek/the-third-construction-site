36e8501ac30d28b58044c667ac9269d310fe404d
carlhernek <53989735+carlhernek@users.noreply.github.com>
2020-09-04 11:15:32 +0200

    Learned to use @Input in Angular
    Will be used to pass in credentials and whatnot

--- src/app/the-third-fake/the-third-fake.component.ts
+++ src/app/the-third-fake/the-third-fake.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-the-third-fake',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./the-third-fake.component.css'],
 })
 export class TheThirdFakeComponent implements OnInit {
+  // Inputs
+  @Input() iType: any;
+  @Input() iAdmin: any;
+
   // User is admin and may edit content
   isAdmin: boolean;
 
@@ -25,18 +29,23 @@ export class TheThirdFakeComponent implements OnInit {
       'Flexitarian air plant readymade williamsburg photo booth biodiesel lomo pork belly knausgaard actually. IPhone hell of iceland, direct trade church-key kombucha polaroid blog fanny pack franzen street art bicycle rights. Butcher tilde 8-bit flexitarian deep v tote bag, cardigan brooklyn messenger bag. Pug viral portland activated charcoal vape. Pork belly scenester glossier quinoa man bun austin salvia umami tilde 3 wolf moon forage kinfolk. Drinking vinegar tumblr slow-carb kale chips fanny pack.',
       'Yr mustache hoodie austin. Craft beer chicharrones echo park glossier squid fingerstache banjo copper mug neutra slow-carb iceland. Cliche heirloom waistcoat, coloring book man braid la croix actually. 90s chartreuse pour-over freegan cold-pressed kogi la croix shaman pitchfork authentic lomo bicycle rights asymmetrical migas. Street art plaid kombucha, tattooed man bun hoodie humblebrag pabst prism cloud bread hella chia. Celiac selfies sustainable gastropub irony green juice tote bag occupy, iceland put a bird on it prism.',
     ];
-    this.cmsType = 2;
+    this.cmsType = 0;
     this.cmsClassDisplay = ['cms-hide', 'cms-hide', 'cms-hide'];
   }
 
   setCmsType = (type: number) => {
     if (type !== 0) {
       this.cmsClassDisplay[type] = 'cms-show';
-      console.log(this.cmsClassDisplay);
     }
   };
 
-  ngOnInit(): void {
+  parseInput = (input: any) => {
+    let output = parseInt(input);
+    this.cmsType = output;
     this.setCmsType(this.cmsType);
+  };
+
+  ngOnInit(): void {
+    this.parseInput(this.iType);
   }
 }
