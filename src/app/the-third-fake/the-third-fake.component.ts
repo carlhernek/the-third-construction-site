931b01adaf61c60f266191d5369298309c0621f8
carlhernek <53989735+carlhernek@users.noreply.github.com>
2020-09-05 14:35:40 +0200

    Controller Structure & Planning

--- src/app/the-third-fake/the-third-fake.component.ts
+++ src/app/the-third-fake/the-third-fake.component.ts
@@ -10,25 +10,37 @@ export class TheThirdFakeComponent implements OnInit {
   @Input() iType: any;
   @Input() iAdmin: any;
 
-  // User is admin and may edit content
+  // 1. User is admin and may edit content
   isAdmin: boolean;
 
-  // Content Variables
+  // 2. Toggles for editing UI
+  editToggle: boolean;
+  editToggleArr: String[];
+
+  // 3. Content Variables
   imageUrl?: string;
   textArr?: string[];
-  cmsType: number; // 0 = Empty, 1 = Image, 2 = Text, 3 = ...etc
 
-  // Class Definitions
+  // 4. CMS Type Definition
+  cmsType: number; // 0 = Empty, 1 = Image, 2 = Text, 3 = ...etc
   cmsClassDisplay: string[];
 
   constructor() {
+    // 1. User is admin
     this.isAdmin = true;
+
+    // 2. Toggles for editing UI
+    this.editToggle = false;
+
+    // 3. Content Variables
     this.imageUrl =
       'https://res.cloudinary.com/carlhernek/image/upload/v1594818036/SAR-demo/Restaurants/orlova-maria-oMTlhdFUhdI-unsplash_tfbudv.jpg';
     this.textArr = [
       'Flexitarian air plant readymade williamsburg photo booth biodiesel lomo pork belly knausgaard actually. IPhone hell of iceland, direct trade church-key kombucha polaroid blog fanny pack franzen street art bicycle rights. Butcher tilde 8-bit flexitarian deep v tote bag, cardigan brooklyn messenger bag. Pug viral portland activated charcoal vape. Pork belly scenester glossier quinoa man bun austin salvia umami tilde 3 wolf moon forage kinfolk. Drinking vinegar tumblr slow-carb kale chips fanny pack.',
       'Yr mustache hoodie austin. Craft beer chicharrones echo park glossier squid fingerstache banjo copper mug neutra slow-carb iceland. Cliche heirloom waistcoat, coloring book man braid la croix actually. 90s chartreuse pour-over freegan cold-pressed kogi la croix shaman pitchfork authentic lomo bicycle rights asymmetrical migas. Street art plaid kombucha, tattooed man bun hoodie humblebrag pabst prism cloud bread hella chia. Celiac selfies sustainable gastropub irony green juice tote bag occupy, iceland put a bird on it prism.',
     ];
+
+    // 4. CMS Type Definition & Classes
     this.cmsType = 0;
     this.cmsClassDisplay = ['cms-hide', 'cms-hide', 'cms-hide'];
   }
