00a9c4c09155d11dc1383a2ac4108b2914a9332e
Carl Hernek <hello@carlhernek.se>
2020-09-21 09:37:27 +0200

    Merge with third cms library

--- src/app/the-third-fake/the-third-fake.component.ts
+++ src/app/the-third-fake/the-third-fake.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TheThirdFakeComponent implements OnInit {
   // Inputs.
   @Input() Auth: any;
+  @Input() CmsID: any;
 
   // 1. User is admin and may edit content.
   isAdmin: boolean;
